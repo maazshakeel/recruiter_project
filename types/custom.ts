1686df351be9883c6e046f62207b87e629234f0a
M. Maaz <maaz.shakeel.bodla@gmail.com>
2023-11-24 22:34:09 +0700

    feat: types directory for custom types

---
+++ types/custom.ts
@@ -0,0 +1,11 @@
+// Create a type for product
+interface Product {
+  nama: string;
+  deskripsi: string;
+  harga: number;
+  stok: number;
+  foto: string;
+  suplier_id: number;
+}
+
+export default Product;
