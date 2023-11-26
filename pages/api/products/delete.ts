a4c7cb717e0a280fa0044f200f175b7a027989ad
M. Maaz <maaz.shakeel.bodla@gmail.com>
2023-11-26 10:19:56 +0700

    feat: create delete route to delete product by its id

--- pages/api/products/delete.ts
+++ pages/api/products/delete.ts
@@ -1,10 +1,27 @@
+// Import necessary dependencies
 import db from "@/lib/db";
 import { NextApiRequest, NextApiResponse } from "next";
 
-// Handler for the Create operation
 export default function deleteProduct(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  return res.status(200).json({ satus: "ok" });
+  // Extract the product ID from the request query parameters
+  const productId = req.query.id;
+  console.log(req.query);
+
+  // Check if the product ID is provided
+  if (!productId || typeof productId !== "string") {
+    return res.status(400).json({ error: "Invalid product ID" });
+  }
+
+  // Use the product ID to delete the corresponding record from the database
+  db.run("DELETE FROM produk WHERE id = ?", [productId], (err) => {
+    if (err) {
+      console.error("Database Error:", err.message);
+      return res.status(500).json({ error: "Failed to delete product" });
+    }
+
+    return res.status(200).json({ message: "Product deleted successfully" });
+  });
 }
