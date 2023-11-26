cfd428605d1ba680f537cf75021b0abfce54b7bd
M. Maaz <maaz.shakeel.bodla@gmail.com>
2023-11-26 10:20:10 +0700

    feat: create update route to update product by its id

--- pages/api/products/update.ts
+++ pages/api/products/update.ts
@@ -1,10 +1,32 @@
 import db from "@/lib/db";
 import { NextApiRequest, NextApiResponse } from "next";
 
-// Handler for the Create operation
 export default function updateProduct(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  return res.status(200).json({ satus: "ok" });
+  // Extract the product ID from the request query parameters
+  const productId = req.query.id;
+
+  // Check if the product ID is provided
+  if (!productId || typeof productId !== "string") {
+    return res.status(400).json({ error: "Invalid product ID" });
+  }
+
+  // Extract the updated product data from the request body
+  const { nama, deskripsi, harga, stok, foto, suplier_id } = req.body;
+
+  // Use the product ID to update the corresponding record in the database
+  db.run(
+    "UPDATE produk SET nama = ?, deskripsi = ?, harga = ?, stok = ?, foto = ?, suplier_id = ? WHERE id = ?",
+    [nama, deskripsi, harga, stok, foto, suplier_id, productId],
+    (err) => {
+      if (err) {
+        console.error("Database Error:", err.message);
+        return res.status(500).json({ error: "Failed to update product" });
+      }
+
+      return res.status(200).json({ message: "Product updated successfully" });
+    },
+  );
 }
