632d355ff67e44382349535ec6c79641251dd4d9
M. Maaz <maaz.shakeel.bodla@gmail.com>
2023-11-26 10:19:32 +0700

    feat: create basic route without uploading file for now for troubleshooting purposes

--- pages/api/products/create.ts
+++ pages/api/products/create.ts
@@ -1,10 +1,22 @@
 import db from "@/lib/db";
 import { NextApiRequest, NextApiResponse } from "next";
 
-// Handler for the Create operation
 export default function createProduct(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  return res.status(200).json({ satus: "ok" });
+  const { nama, deskripsi, harga, stok, foto, suplier_id } = req.body;
+
+  db.run(
+    "INSERT INTO produk (nama, deskripsi, harga, stok, foto, suplier_id) VALUES (?, ?, ?, ?, ?, ?)",
+    [nama, deskripsi, harga, stok, foto, suplier_id],
+    (err) => {
+      if (err) {
+        console.error("Database Error:", err.message);
+        return res.status(500).json({ error: "Failed to create product" });
+      }
+
+      return res.status(201).json({ message: "Product created successfully" });
+    },
+  );
 }
