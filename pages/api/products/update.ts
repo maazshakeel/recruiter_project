9c612cae14c97cdad041ddb79dd367fa76bdfa6a
maazshakeel <maaz.shakeel.bodla@gmail.com>
2023-11-27 00:21:31 +0700

    feat(CRUD): :art: Deleting the file completely(there was a bug before)
    Delete file completely, Update correctly

--- pages/api/products/update.ts
+++ pages/api/products/update.ts
@@ -1,34 +1,108 @@
-import db from "@/lib/db";
-import { NextApiRequest, NextApiResponse } from "next";
+// import db from "@/lib/db";
+// import { NextApiRequest, NextApiResponse } from "next";
 
-export default function updateProduct(
-  req: NextApiRequest,
-  res: NextApiResponse,
-) {
-  if (req.method === "POST") {
-    const productId = req.query.id;
+// export const config = {
+//   api: {
+//     bodyParser: false,
+//   },
+// };
 
-    if (!productId || typeof productId !== "string") {
-      return res.status(400).json({ error: "Invalid product ID" });
-    }
+// export default function updateProduct(
+//   req: NextApiRequest,
+//   res: NextApiResponse,
+// ) {
+//   if (req.method === "POST") {
+//     const productId = req.query.id;
 
-    const { nama, deskripsi, harga, stok, foto, suplier_id } = req.body;
+//     if (!productId || typeof productId !== "string") {
+//       return res.status(400).json({ error: "Invalid product ID" });
+//     }
 
-    db.run(
-      "UPDATE produk SET nama = ?, deskripsi = ?, harga = ?, stok = ?, foto = ?, suplier_id = ? WHERE id = ?",
-      [nama, deskripsi, harga, stok, foto, suplier_id, productId],
-      (err) => {
-        if (err) {
-          console.error("Database Error:", err.message);
-          return res.status(500).json({ error: "Failed to update product" });
+//     const { nama, deskripsi, harga, stok, foto, suplier_id } = req.body;
+
+//     db.run(
+//       "UPDATE produk SET nama = ?, deskripsi = ?, harga = ?, stok = ?, foto = ?, suplier_id = ? WHERE id = ?",
+//       [nama, deskripsi, harga, stok, foto, suplier_id, productId],
+//       (err) => {
+//         if (err) {
+//           console.error("Database Error:", err.message);
+//           return res.status(500).json({ error: "Failed to update product" });
+//         }
+
+//         return res
+//           .status(200)
+//           .json({ message: "Product updated successfully" });
+//       },
+//     );
+//   } else {
+//     return res.status(404).json({ status: "Wrong method!" });
+//   }
+// }
+
+import type { NextApiRequest, NextApiResponse } from "next";
+import path from "path";
+import { CustomNextApiRequest, Product } from "@/types/custom";
+import { moveFile } from "@/utils/moveFile";
+import { createDirectory } from "@/utils/createDirectory";
+import { parseFormData } from "@/utils/parseFormData";
+import {
+  getLastProductId,
+  insertProduct,
+  updateProduct,
+} from "@/services/crud.service";
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const handler = async (req: CustomNextApiRequest, res: NextApiResponse) => {
+  let status = 200;
+  const inputData: Product | any = {};
+
+  const lastProductId = await getLastProductId();
+  const newProductId = lastProductId !== null ? lastProductId + 1 : 1;
+
+  try {
+    const files = await parseFormData(req, inputData);
+
+    if (files?.length) {
+      const targetPath = path.join(process.cwd(), `public/uploads/products/`);
+      const n = `${req.query.id}.${files[0][1]?.originalFilename?.split(
+        "."
+      )[1]}`;
+      console.log(n);
+      await createDirectory(targetPath);
+
+      for (const file of files) {
+        const tempPath = file[1].filepath;
+        if (!file[1].originalFilename) {
+          return res.status(400).send("Error: Original filename missing.");
         }
 
-        return res
-          .status(200)
-          .json({ message: "Product updated successfully" });
-      },
-    );
-  } else {
-    return res.status(404).json({ status: "Wrong method!" });
+        const targetFilePath = path.join(targetPath, n);
+        console.log(targetFilePath);
+
+        await moveFile(tempPath, targetFilePath);
+      }
+
+      const file_user = `uploads/products/${
+        req.query.id
+      }.${files[0][1]?.originalFilename?.split(".")[1]}`;
+      inputData["path"] = file_user;
+
+      await updateProduct(1, inputData);
+
+      return res.status(201).json({ message: "Product updated successfully" });
+    } else {
+      return res.status(404).send("Upload file.");
+    }
+  } catch (error: any) {
+    console.error("Error:", error.message);
+    status = 500;
+    return res.status(status).json({ error: "Internal Server Error" });
   }
-}
+};
+
+export default handler;
